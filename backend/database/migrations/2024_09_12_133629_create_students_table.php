64fb9032f8f7d633e2178d53830217ddef6962b7
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2024-09-13 18:36:40 +0100

    updated database

--- backend/database/migrations/2024_09_12_133629_create_students_table.php
+++ backend/database/migrations/2024_09_12_133629_create_students_table.php
@@ -9,9 +9,11 @@ class CreateStudentsTable extends Migration
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id('student_id');
+            $table->id('student_id')->unique();
             $table->foreignId('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->string('pc_brand');
             $table->string('serial_number')->unique();
+            $table->string('pc_color');
             $table->string('qr_code')->nullable();
             $table->timestamps();
         });
