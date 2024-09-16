f713d41441c6063258d0c2fb730a327d4f046945
eteaby <samrawitfikremariam@gmail.com>
2024-09-16 21:35:24 +0300

    students and admin section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate
+import Students from './Students';
+import Admins from './Admins';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/students" element={<Students />} />
+        <Route path="/admins" element={<Admins />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
