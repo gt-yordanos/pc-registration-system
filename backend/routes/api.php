7feb03375649b497bf5d470b3dc1210b005f4302
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2024-09-20 16:39:33 +0100

    updataed route path

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PcController;
-use App\Http\Controllers\QrCodeController;
+use App\Http\Controllers\QrController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -59,15 +59,15 @@ Route::group(['prefix' => 'api'], function () {
     Route::delete('/admins/{id}', [AdminController::class, 'delete']);
 
     // PC routes
-    Route::get('/pcs', [PCController::class, 'index']);
-    Route::get('/pcs/{id}', [PCController::class, 'show']);
-    Route::post('/pcs', [PCController::class, 'store']);
-    Route::put('/pcs/{id}', [PCController::class, 'update']);
-    Route::delete('/pcs/{id}', [PCController::class, 'delete']);
+    Route::get('/pcs', [PcController::class, 'index']);
+    Route::get('/pcs/{id}', [PcController::class, 'show']);
+    Route::post('/pcs', [PcController::class, 'store']);
+    Route::put('/pcs/{id}', [PcController::class, 'update']);
+    Route::delete('/pcs/{id}', [PcController::class, 'delete']);
 
     // QR Code routes
-    Route::post('/qrcodes/generate', [QRCodeController::class, 'generate']);
-    Route::post('/qrcodes/scan', [QRCodeController::class, 'scan']);
+    Route::post('/qrcodes/generate', [QrController::class, 'generate']);
+    Route::post('/qrcodes/scan', [QrController::class, 'scan']);
 });
 
 
